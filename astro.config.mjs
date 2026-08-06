1f609de41fee1ecb5d1a2fbac75beab16526d173
sohamy <61020364+sohamy@users.noreply.github.com>
2026-08-06 16:13:21 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://sohamy.github.io',
-  base: '/butterfly-community',
+  base: '/Butterfly',
   output: 'static',
 });
